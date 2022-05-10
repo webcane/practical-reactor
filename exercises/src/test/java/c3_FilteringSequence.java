d68887b1984698a0586ee08beda940425cab465b
Mikhail Niedre <mikhail.niedre@quantori.com>
2022-05-10 20:44:53 +0300

    c3 my solutions

--- exercises/src/test/java/c3_FilteringSequence.java
+++ exercises/src/test/java/c3_FilteringSequence.java
@@ -27,8 +27,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     @Test
     public void girls_are_made_of_sugar_and_spice() {
         Flux<String> shortListed = popular_girl_names_service()
-                //todo: change this line only
-                ;
+            .filter(n -> n.length() <= 4);
 
         StepVerifier.create(shortListed)
                     .expectNext("Emma", "Ava", "Mia", "Luna", "Ella")
@@ -41,9 +40,10 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void needle_in_a_haystack() {
+//        Flux<String> strings = mashed_data_service().ofType(String.class);
         Flux<Object> strings = mashed_data_service()
-                //todo: change this line only
-                ;
+            .ofType(String.class)
+            .map(String::valueOf);
 
         StepVerifier.create(strings)
                     .expectNext("1", "String.class")
@@ -56,8 +56,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     @Test
     public void economical() {
         Flux<String> items = duplicated_records_service()
-                //todo: change this line only, use only one operator
-                ;
+            .distinct();
 
         StepVerifier.create(items)
                     .expectNext("1", "2", "3", "4", "5")
@@ -72,9 +71,9 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void watch_out_for_the_spiders() {
-        //todo: change code as you need
-        Mono<String> firstResult = Mono.empty();
-        fragile_service();
+        Mono<String> firstResult = fragile_service()
+            //.single(); // throws exception if more then 1
+                .elementAt(0); // index for 0
 
         //don't change code below
         StepVerifier.create(firstResult)
@@ -88,8 +87,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     @Test
     public void dont_take_more_then_you_need() {
         Flux<Integer> numbers = number_service()
-                //todo: change this line only
-                ;
+            .take(100, true); // take first 100 then stop
 
         StepVerifier.create(numbers)
                     .expectNextCount(100)
@@ -102,8 +100,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     @Test
     public void not_a_binary_search() {
         Flux<Integer> numbers = number_service()
-                //todo: change this line only
-                ;
+            .takeLast(100);
 
         StepVerifier.create(numbers)
                     .expectNextMatches(i -> i >= 200)
@@ -117,8 +114,8 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     @Test
     public void golden_middle() {
         Flux<Integer> numbers = number_service()
-                //todo: do your changes here
-                ;
+            .skip(100)
+            .take(100, true);
 
         StepVerifier.create(numbers)
                     .expectNextMatches(i -> i >= 100)
